a9fa2f47fbc288e00e04056f75cff5a4c88e90a3
Luu Quang Minh <pmast1234567890@gmail.com>
2024-10-01 14:24:03 +0700

    commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Nguyen Quoc Chung</Text>
-      <Text>Nguyen Quoc Chung</Text>
-      <Text>Nguyen Quoc Chung</Text>
-      <Text>Nguyen Quoc Chung</Text>
-      <Text>Luu Quang Minh</Text>
-      <StatusBar style="auto" />
-      <Text> Phan Hoang Minh Phuoc</Text>
-      <Text>Hello</Text> 
+        <Text style={styles.title}>Sign Up</Text>
+        <View style={styles.inputContainer}>
+              <TextInput placeholder='Username' style={styles.input} />
+              <TextInput placeholder='Email' style={styles.input} />
+              <TextInput placeholder='Password' style={styles.input} />
+              <TouchableOpacity style={styles.button}><Text style={styles.textButton}>Register</Text></TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -23,8 +18,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent:'center',
     alignItems: 'center',
+    gap: 40
+  },
+  title: {
+    fontSize: 40,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  inputContainer: {
+    width: '100%',
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: 250,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    fontSize: 15,
+    margin: 6
+  },
+  button: {
+    backgroundColor: '#0766ff',
+    borderRadius: 10,
+    height: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5
+  },
+  textButton: {
+    color: 'white',
+    fontSize: 20,
   },
 });
